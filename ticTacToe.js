3ff25f3f84ef03c7f40a0960deb96dde29a14498
Amanda Reilly <amandamichellereilly@gmail.com>
2018-12-30 00:20:57 -0800

    Add check for win functionality.

--- ticTacToe.js
+++ ticTacToe.js
@@ -2,7 +2,7 @@ const readline = require('readline');
 
 class Game {
     constructor() {
-        this.board = this._makeBoard();
+        this._resetGame();
         this._outputStrs = {
             intro: 'Let\'s play Tic Tac Toe! The first player to get three in a row (horizontal, vertical, or diagonal) wins! \r',
             instructions: 'When it\'s your turn to move, enter \'row column \' with a space in between.  For example, to target the top left space, enter \'1 1\'.',
@@ -14,7 +14,6 @@ class Game {
             playerMovePrompt: 'Your turn!  Please select a space to place your token.',
             gameOver: 'Game Over!'
         }
-        this.gameOver = false;
     }
     start() {
         console.log(this._outputStrs.intro);
@@ -43,10 +42,18 @@ class Game {
                     if(!this.gameOver) {
                         console.log(this._outputStrs.playerMovePrompt);
                         this._printBoard();
+                    } else if(this.gameWon) {
+                        this._printBoard();
+                        console.log(`${this.gameWinner} won!!`);
+                        setTimeout(this._stopListening.bind(this), 1000);
                     } else {
                         console.log(this._outputStrs.gameOver);
                         this._stopListening();
                     }
+                } else if (this.gameWon) {
+                    this._printBoard();
+                    console.log(`${this.gameWinner} won!!`);
+                    setTimeout(this._stopListening.bind(this), 1000);
                 } else {
                     console.log(this._outputStrs.gameOver);
                     this._stopListening();
@@ -55,8 +62,12 @@ class Game {
         }
     }
 
-    _makeBoard() {
-        return [['-', '-', '-'], ['-', '-', '-'], ['-', '-', '-']];
+    _resetGame() {
+        this.board = [['-', '-', '-'], ['-', '-', '-'], ['-', '-', '-']];
+        this.gameOver = false;
+        this.winPossible = true;
+        this.gameWon = false;
+        this.gameWinner = null;
     }
 
     _listen() {
@@ -84,11 +95,29 @@ class Game {
     _addToken(token, row, column) {
         if(this.board[row][column] === '-') {
             this.board[row][column] = token;
+            this._checkForWin(token, row, column);
             this._checkBoard();
             return true;
         } else {
             return false;
         }
+    }
+
+    _checkForWin(token, row, column) {
+        // if we check after each move
+        // then we can only win in the
+        // row or column of the last move
+        // - it's easiest to just check
+        // both diagonals every time
+        const rowWin = (this.board[row][0] === token && this.board[row][1] === token && this.board[row][2] === token);
+        const colWin = (this.board[0][column] === token && this.board[1][column] === token && this.board[2][column] === token);
+        const diagWin = (this.board[1][1] === token && ((this.board[0][0] === token && this.board[2][2] === token) || (this.board[0][2] === token && this.board[2][0] === token)));
+        if(rowWin || colWin || diagWin) {
+            this.gameWon = true;
+            this.gameWinner = token;
+            this.gameOver = true;
+        }
+        
     }
 
     _checkBoard() {
